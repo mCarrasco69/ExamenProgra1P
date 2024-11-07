3625683f7b273ec75a98fd325e7adb9c1963384a
J Miguel MC <162650216+mCarrasco69@users.noreply.github.com>
2024-11-06 19:09:46 -0600

    Update Asignatura.cs

--- ExamenProgra1P/Asignatura.cs
+++ ExamenProgra1P/Asignatura.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace ExamenProgra1P
+using ExamenProgra1P;
+
+public class Asignatura : Alumno, IAsignatura
 {
-    internal class Asignatura
+    public int N1 { get; set; }
+    public int N2 { get; set; }
+    public int N3 { get; set; }
+    public string NombreAsignatura { get; set; }
+    public string Horario { get; set; }
+    public string NombreDocente { get; set; }
+
+    public double CalcularNotaFinal()
     {
+        return N1 + N2 + N3;
     }
+
+    public double CalcularNotaFinal(double n1, double n2, double n3)
+    {
+        return n1 + n2 + n3;
+    }
+
 }

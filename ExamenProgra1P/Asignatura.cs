17c9b9035e76d116df6efdb495d79db56c16cfa8
J Miguel MC <162650216+mCarrasco69@users.noreply.github.com>
2024-11-06 20:03:26 -0600

    Update Asignatura.cs

--- ExamenProgra1P/Asignatura.cs
+++ ExamenProgra1P/Asignatura.cs
@@ -24,14 +24,28 @@ public class Asignatura : Alumno, IAsignatura
     public string MensajeNotaFinal(double notaFinal)
     {
         if (notaFinal < 60)
-            return "Reprobado";
+            return "Ta frito raza";
         else if (notaFinal < 80)
-            return "Bueno";
+            return "De wasa paso";
         else if (notaFinal < 90)
-            return "Muy Bueno";
+            return "Fresa raza paso";
         else
-             return "Sobresaliente";
+             return "Huy una birria ocupa la merese";
     }
 
-    public 
+    public void Imprimir()
+    {
+        double notaFinal1 = CalcularNotaFinal();
+        double notaFinal2 = CalcularNotaFinal(N1, N2, N3);
+        Console.WriteLine();
+        Console.WriteLine($"Nombre del Alumno: {NombreAlumno}");
+        Console.WriteLine($"Número de Cuenta: {NumeroCuenta}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Asignatura: {NombreAsignatura}");
+        Console.WriteLine($"Horario: {Horario}");
+        Console.WriteLine($"Docente: {NombreDocente}");
+        Console.WriteLine($"Nota Final (sin parámetros): {notaFinal1} - {MensajeNotaFinal(notaFinal1)}%");
+        Console.WriteLine($"Nota Final (con parámetros): {notaFinal2} - {MensajeNotaFinal(notaFinal2)}%");
+        Console.WriteLine("***********************************************");
+    }
 }

635437ea30c3836511be3ab59716177b51b9bd82
J Miguel MC <162650216+mCarrasco69@users.noreply.github.com>
2024-11-06 18:56:36 -0600

    Implementando interfaz

---
+++ ExamenProgra1P/IAsignatura.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ExamenProgra1P
+{
+    public interface IAsignatura
+    {
+        double CalcularNotaFinal();
+        double CalcularNotaFinal(double n1, double n2 , double n3);
+
+        string MensajeNotaFinal(double notaFinal);
+
+        void Imprimir();
+
+    }
+}
